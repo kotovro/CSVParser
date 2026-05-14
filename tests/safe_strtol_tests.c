517010bf65d739d10886b673f2f468f3665f782e
Konstantin Voronetsky <kotvoro@gmail.com>
2026-05-14 16:31:26 +0000

    Adds strtol tests

---
+++ tests/safe_strtol_tests.c
@@ -0,0 +1,40 @@
+#include "safe_strtol_tests.h"
+#include "asserts.h"
+#include "../src/parserutils.h"
+
+void test_valid_number(void);
+void test_invalid_number(void);
+void test_empty_string(void);
+void test_null_string(void);    
+
+void run_safe_strtol_tests(void) {
+    test_valid_number();
+    test_invalid_number();
+    test_empty_string();
+    test_null_string();
+}
+
+void test_valid_number(void) {
+    long out_val;
+    bool result = safe_strtol("12345", &out_val);
+    ASSERT_BOOL_EQ(result, true);
+    ASSERT_NUM_EQ(12345, out_val);
+}
+
+void test_invalid_number(void) {
+    long out_val;
+    bool result = safe_strtol("abc", &out_val);
+    ASSERT_BOOL_EQ(result, false);
+}
+
+void test_empty_string(void) {
+    long out_val;
+    bool result = safe_strtol("", &out_val);
+    ASSERT_BOOL_EQ(result, false);
+}
+
+void test_null_string(void) {
+    long out_val;
+    bool result = safe_strtol(NULL, &out_val);
+    ASSERT_BOOL_EQ(result, false);
+}
---
+++ tests/safe_strtol_tests.h
@@ -0,0 +1,6 @@
+#ifndef SAFE_STRTOL_TESTS_H
+#define SAFE_STRTOL_TESTS_H
+
+void run_safe_strtol_tests(void);
+
+#endif // SAFE_STRTOL_TESTS_H
